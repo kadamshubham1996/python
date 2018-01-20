c6e7735c9076bb24b534713ff1b54c8403c97391
rckstr <kadamshubham66@gmail.com>
2018-01-20 12:21:53 +0530

    first update

---
+++ fizzbuzz.py
@@ -0,0 +1,9 @@
+n=input("entyer the number")
+if (n % 3 == 0 | n % 5 == 0):
+    print "FIZZBUZZ"
+elif (n % 3 == 0):
+    print "FIZZ"
+elif (n % 5 == 0):
+    print "BUZZ"
+
+
---
+++ prime.py
@@ -0,0 +1,11 @@
+n=input("enter the number")
+temp=0
+for i in range(2,n/2):
+   a=n%2
+   temp=a
+
+
+if  (temp == 0):
+    print "not prime"
+else:
+    print "number is prime"
